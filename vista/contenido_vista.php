4ddd79593e8067f69ff3d1a4303cdc017a1ded34
nicollevalladares <nicollevalladares@gmail.com>
2019-02-14 20:57:10 -0600

    cambio

--- vista/contenido_vista.php
+++ vista/contenido_vista.php
@@ -9,13 +9,10 @@
 					</div>
 						<p><img src="img/icono.svg">
 						</p><br/><br/>
-						<p>Bienvenido a <b>....</b>! un Sistema útil para consultorios medicos y/o medicos independientes.</p>
+						<p>Bienvenido a <b>....</b>! un Sistema útil para consultorios odontológicos y/o odontológicos independientes.</p>
 						<br/><br/>
 						<h3>Nuestras funciones</h3><br/>
-						<p>	- Gestion de Citas <br/>
-							- Gestion de Odontólogos <br/>
-							- Gestion de Pacientes <br/>
-							- Gestion de Usuarios con Acceso al Sistema <br/>
+						<p>	................
 						</p>
 				</article>
 	</section>
